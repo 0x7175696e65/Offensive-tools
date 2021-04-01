b7c4b78053dd779a6b1b8675f2cd8ae81e8d2fb7
0x7175696e65 <79869057+0x7175696e65@users.noreply.github.com>
2021-04-01 03:12:02 +0300

    Update pwop-rs.cs

--- PwoP/pwop-rs.cs
+++ PwoP/pwop-rs.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
 
+namespace PwoP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            RunspaceInvoke ri = new RunspaceInvoke(rs);
+            Pipeline pipeL = rs.CreatePipeline();
+
+            pipeL.Commands.AddScript(args[0]);
+            pipeL.Commands.Add("Out-String");
+
+            Collection<PSObject> output = pipeL.Invoke();
+
+            rs.Close();
+            foreach(PSObject line in output)
+            {
+                sb.AppendLine(line.ToString());
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
