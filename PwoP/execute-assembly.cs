5d95b7caeae7d3627b419e2e6e00845cc71da385
0x7175696e65 <79869057+0x7175696e65@users.noreply.github.com>
2021-04-01 03:12:25 +0300

    Update execute-assembly.cs

--- PwoP/execute-assembly.cs
+++ PwoP/execute-assembly.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
 
+
+namespace DotNetLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] binary = Convert.FromBase64String(args[0]);
+            List<string> processArgs = new List<string>();
+            processArgs.Add(args[1]);
+
+            try
+            {
+                Assembly assembly = Assembly.Load(binary);
+                MethodInfo mI = assembly.EntryPoint;
+                Console.WriteLine(mI.Name);
+
+                assembly.CreateInstance(mI.Name);
+                mI.Invoke(null, new object[] { (object[])processArgs.ToArray() });
+
+            }
+            catch
+            {
+                Console.WriteLine("Invalid data");
+            }
+
+        }
+    }
+}
